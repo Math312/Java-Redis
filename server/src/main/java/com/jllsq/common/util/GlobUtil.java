53a6b66f4971a0968b87c182e9c5dae7eecd7865
Math312 <894688078@qq.com>
2019-09-29 11:47:30 +0800

    fix glob \ bug

--- server/src/main/java/com/jllsq/common/util/GlobUtil.java
+++ server/src/main/java/com/jllsq/common/util/GlobUtil.java
@@ -1,58 +1,45 @@
 package com.jllsq.common.util;
 
-import sun.nio.fs.Globs;
-
 public class GlobUtil {
 
-    public static boolean match(byte[] string,int stringLen,byte[] pattern, int patternLen) {
-        return match(string,stringLen,0,pattern,patternLen,0);
+    public static boolean match(byte[] string, int stringLen, byte[] pattern, int patternLen) {
+        return match(string, stringLen, 0, pattern, patternLen, 0);
     }
 
-    private static boolean match(byte[] string,int stringLen,int stringIndex,byte[] pattern, int patternLen,int patternIndex) {
+    private static boolean match(byte[] string, int stringLen, int stringIndex, byte[] pattern, int patternLen, int patternIndex) {
         if (stringIndex == stringLen && patternIndex == patternLen) {
             return true;
         }
         if (stringIndex < stringLen && patternIndex == patternLen) {
             return false;
         }
-        if (patternIndex < patternLen && pattern[patternIndex] == '*') {
-            if (stringIndex < stringLen){
-                return match(string,stringLen,stringIndex+1,pattern,patternLen,patternIndex)
-                        || match(string,stringLen,stringIndex,pattern,patternLen,patternIndex+1)
-                        || match(string,stringLen,stringIndex+1,pattern,patternLen,patternIndex+1);
-            } else {
-                return match(string,stringLen,stringIndex,pattern,patternLen,patternIndex+1);
-            }
-        }
-        if (pattern[patternIndex] == '?' || string[stringIndex] == pattern[patternIndex]) {
-            return match(string,stringLen,stringIndex+1,pattern,patternLen,patternIndex+1);
-        }
+
         if (pattern[patternIndex] == '[') {
             int index = patternIndex;
             boolean wrong = true;
             int start;
             int end;
             boolean range = false;
-            while(index < patternLen) {
+            while (index < patternLen) {
                 if (pattern[index] == ']') {
                     wrong = false;
                     break;
-                }else {
-                    if (pattern[index] == '-'){
+                } else {
+                    if (pattern[index] == '-') {
                         range = true;
                     }
-                    index ++;
+                    index++;
                 }
             }
             if (wrong) {
                 throw new IllegalArgumentException();
             }
             if (range) {
-                if (index-patternIndex < 4) {
+                if (index - patternIndex < 4) {
                     throw new IllegalArgumentException();
                 }
-                start = pattern[patternIndex+1];
-                end = pattern[index-1];
+                start = pattern[patternIndex + 1];
+                end = pattern[index - 1];
                 if (string[stringIndex] >= start && string[stringIndex] <= end) {
                     return match(string, stringLen, stringIndex + 1, pattern, patternLen, index + 1);
                 } else {
@@ -60,7 +47,7 @@ public class GlobUtil {
                 }
             }
             boolean result = false;
-            for (int i = patternIndex+1;i < index;i ++){
+            for (int i = patternIndex + 1; i < index; i++) {
                 if (string[stringIndex] == pattern[i]) {
                     result = true;
                 }
@@ -70,13 +57,26 @@ public class GlobUtil {
             }
             return false;
         }
-        if (pattern[patternIndex] == '\\') {
-            if (patternIndex+1 < patternLen || pattern[patternIndex+1] == '*' || pattern[patternIndex+1] == '?' || pattern[patternIndex+1] == '[') {
-                return string[stringIndex]==pattern[patternIndex+1]&&match(string, stringLen, stringIndex+1, pattern, patternLen, patternIndex+2);
+        if ( stringIndex < stringLen && pattern[patternIndex] == '\\') {
+            if (patternIndex + 1 < patternLen && (pattern[patternIndex + 1] == '*' || pattern[patternIndex + 1] == '?' || pattern[patternIndex + 1] == '[')) {
+                return string[stringIndex] == pattern[patternIndex + 1] && match(string, stringLen, stringIndex + 1, pattern, patternLen, patternIndex + 2);
+            } else {
+                return string[stringIndex] == pattern[patternIndex] && match(string, stringLen, stringIndex + 1, pattern, patternLen, patternIndex + 1);
             }
-            else {
-                return string[stringIndex]==pattern[patternIndex]&&match(string, stringLen, stringIndex+1, pattern, patternLen, patternIndex+1);
+        }
+
+        if (patternIndex < patternLen && pattern[patternIndex] == '*') {
+            if (stringIndex < stringLen) {
+                boolean r1 =  match(string, stringLen, stringIndex + 1, pattern, patternLen, patternIndex);
+                boolean r2 = match(string, stringLen, stringIndex, pattern, patternLen, patternIndex + 1);
+                boolean r3 = match(string, stringLen, stringIndex + 1, pattern, patternLen, patternIndex + 1);
+                return r1 || r2 || r3;
+            } else {
+                return match(string, stringLen, stringIndex, pattern, patternLen, patternIndex + 1);
             }
+        }
+        if (stringIndex < stringLen && (pattern[patternIndex] == '?' || string[stringIndex] == pattern[patternIndex])) {
+            return match(string, stringLen, stringIndex + 1, pattern, patternLen, patternIndex + 1);
         }
         return false;
 
