d79440f975a4f36ffec8e3ddde7a502d79a46a64
Math312 <894688078@qq.com>
2020-04-25 19:40:17 +0800

    [更新] 消除命令处理中的拷贝操作

--- server/src/main/java/com/jllsq/command/impl/AppendCommand.java
+++ server/src/main/java/com/jllsq/command/impl/AppendCommand.java
@@ -10,7 +10,6 @@ import com.jllsq.common.entity.RedisDb;
 import com.jllsq.common.entity.RedisObject;
 import com.jllsq.config.Shared;
 import com.jllsq.holder.RedisServerStateHolder;
-import org.apache.commons.lang3.SerializationUtils;
 
 import static com.jllsq.holder.RedisServerObjectHolder.REDIS_STRING;
 
@@ -20,30 +19,37 @@ public class AppendCommand extends RedisCommand {
         super(new SDS("append"),2);
     }
 
+    private int processResultLabel = 0;
+
     @Override
     public RedisObject process(RedisClient client) {
         RedisDb db = client.getDb();
         DictEntry entry = db.getDict().find(client.getArgv()[1]);
         if (entry == null) {
-            RedisObject key = SerializationUtils.clone(client.getArgv()[1]);
-            RedisObject value = SerializationUtils.clone(client.getArgv()[2]);
+            RedisObject key = client.getArgv()[1];
+            RedisObject value = client.getArgv()[2];
             boolean addResult = db.getDict().add(key, value);
             if (addResult) {
+                processResultLabel = 0;
                 RedisServerStateHolder.getInstance().incrDirty();
-                return db.getDict().find(client.getArgv()[1]).getValue();
+                return value;
             } else {
+                processResultLabel = 1;
                 return Shared.getInstance().getCzero();
             }
         } else {
             RedisObject value = entry.getValue();
             if (value.getType() != REDIS_STRING) {
+                processResultLabel = -1;
                 return Shared.getInstance().getWrongtypeerr();
             } else {
                 try {
                     value.setPtr(((SDS) (entry.getValue().getPtr())).append(((SDS) (client.getArgv()[2].getPtr()))));
                     RedisServerStateHolder.getInstance().incrDirty();
+                    processResultLabel = 2;
                 } catch (SDSMaxLengthException e) {
                     e.printStackTrace();
+                    processResultLabel = -1;
                     return Shared.getInstance().getErr();
                 }
                 return entry.getValue();
@@ -54,8 +60,13 @@ public class AppendCommand extends RedisCommand {
     @Override
     public void recycleRedisObject(RedisClient client) {
         redisServerObjectHolder.deleteObject(client.getArgv()[0]);
-        redisServerObjectHolder.deleteObject(client.getArgv()[1]);
-        redisServerObjectHolder.deleteObject(client.getArgv()[2]);
+        switch (processResultLabel) {
+            case 0:
+                break;
+            default:
+                redisServerObjectHolder.deleteObject(client.getArgv()[1]);
+                redisServerObjectHolder.deleteObject(client.getArgv()[2]);
+        }
     }
 
     @Override
